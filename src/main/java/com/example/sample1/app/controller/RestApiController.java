d1ef52bb1d7acf4de2d6877c2f25f0919b6c5eed
yuuki-train <yuuki323-sld51@kcf.biglobe.ne.jp>
2020-12-01 16:49:53 +0900

    api-call-sample modified

--- src/main/java/com/example/sample1/app/controller/RestApiController.java
+++ src/main/java/com/example/sample1/app/controller/RestApiController.java
@@ -1,12 +1,10 @@
 package com.example.sample1.app.controller;
 
 import com.example.sample1.app.resource.BirthStone;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("api/sample")
 public class RestApiController {
 
